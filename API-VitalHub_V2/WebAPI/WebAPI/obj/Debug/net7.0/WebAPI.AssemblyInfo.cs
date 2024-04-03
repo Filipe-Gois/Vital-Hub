2021bff579531a95baa31acc2b89d88d2eb3ea6f
Filipe <fythoy@gmail.com>
2024-04-03 15:24:16 -0300

    novo

--- API-VitalHub_V2/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ API-VitalHub_V2/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2b4905e5edd6e9cbc6793675b93c9f33615188")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e153a8335775e120f1d619005850468cbc46e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
